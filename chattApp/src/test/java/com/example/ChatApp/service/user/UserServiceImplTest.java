737991c607f28c8247326bced68ba119d77ce807
Mamy <xingyanjintian@gmail.com>
2025-04-23 15:45:10 +0900

    テストケースを作成

---
+++ chattApp/src/test/java/com/example/ChatApp/service/user/UserServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.example.ChatApp.service.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceImplTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
