4b13dfadfb42c2700d69109d355aa4998588ffb1
Mamy <xingyanjintian@gmail.com>
2025-04-23 15:32:04 +0900

    entityのidをUUIDで生成するよう改修

--- chattApp/src/main/java/com/example/ChatApp/entity/UserEntity.java
+++ chattApp/src/main/java/com/example/ChatApp/entity/UserEntity.java
@@ -2,6 +2,8 @@ package com.example.ChatApp.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -12,6 +14,7 @@ import lombok.Data;
 @Table(name = "users")
 public class UserEntity {
 	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
 	@Column(name = "id")
 	private String id;
 
